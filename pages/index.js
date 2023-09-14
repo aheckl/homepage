6d895cbe5264a4781de5f57b13ff3dcd7964c0c1
aheckl <a.heckl@hotmail.de>
2023-09-14 15:09:31 +0200

    change Linkedin URL

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,7 @@ const Home = () => {
               </a>
             </li>
             <li>
-              <a href="https://www.linkedin.com/in/andreas-heckl-9ab02828b/" className="text-blue-600 hover:underline">
+              <a href="https://www.linkedin.com/in/andreas-heckl/" className="text-blue-600 hover:underline">
                 <FontAwesomeIcon icon={faLinkedinBrands} className="mr-2" />
                 Linkedin
               </a>
